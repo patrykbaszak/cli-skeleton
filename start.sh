1acb4a66c6cf1437cb4d5e4fa5de41b6d3333899
patrykbaszak <patryk.baszak@gmail.com>
2023-11-17 23:02:12 +0100

    added finishing script

--- start.sh
+++ start.sh
@@ -4,3 +4,7 @@ composer create-project pbaszak/skeleton --no-interaction
 
 rm -rf skeleton/src
 cp -r src skeleton/src
+
+rm -rf .gitignore composer.json composer.lock README.md LICENSE .git vendor start.sh
+
+mv skeleton/{,.[^.]}* ./
